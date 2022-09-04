a3eece5ec5a6c976d3d990a20c4389dbd3c3f216
Durgeshsharma13 <112408094+Durgeshsharma13@users.noreply.github.com>
2022-09-04 15:40:00 +0530

    Add files via upload

---
+++ Max.java
@@ -0,0 +1,15 @@
+class Max 
+{
+    public static void main(String args[])
+    {
+        int a=190 ,b=20;
+        if (a>b)
+        {
+            System.out.print(a);
+        }
+       else
+        {
+            System.out.print(b);
+        }
+    }
+}
